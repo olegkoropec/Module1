53c562bb6f8c3554355fe37debda2a4a04cf2b94
Oleh <k.olegu23@gmail.com>
2023-06-22 20:57:34 +0300

    first change of file

--- src/InputFile.java
+++ src/InputFile.java
@@ -1,14 +1,13 @@
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
-import java.util.List;
+
 
 public class InputFile {
     public static void main(String[] args) throws IOException {
 
-        Character[] letters = {'.', ',', '”', ':', '-', '—', '!', '?', ' ', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+        Character[] letters = {'.', ',', '”', ':', '-', '—', '!', '?', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
                 'А', 'Б', 'В', 'Г', 'Ґ', 'Д', 'Е', 'Є', 'Ж', 'З', 'И', 'І', 'Ї', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь', 'Ю', 'Я',
                 'а', 'б', 'в', 'г', 'ґ', 'д', 'е', 'є', 'ж', 'з', 'и', 'і', 'ї', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ю', 'я'};
         String inputFile = null;
@@ -36,7 +35,5 @@ public class InputFile {
         } catch (NoSuchFileException e) {
             System.out.println("file with name " + inputFile + " do not exit");
         }
-
     }
-
 }

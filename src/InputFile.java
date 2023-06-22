2cc2d732ccc80a06b0248863b78d34824d7a1b87
Oleh <k.olegu23@gmail.com>
2023-06-22 22:49:42 +0300

    add method decoder

--- src/InputFile.java
+++ src/InputFile.java
@@ -1,39 +1,70 @@
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.NoSuchFileException;
-import java.nio.file.Path;
-
+import java.util.Scanner;
 
 public class InputFile {
-    public static void main(String[] args) throws IOException {
+    Character[] letters = {'.', ',', '”', ':', '-', '—', '!', '?', ' ', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            'А', 'Б', 'В', 'Г', 'Ґ', 'Д', 'Е', 'Є', 'Ж', 'З', 'И', 'І', 'Ї', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь', 'Ю', 'Я',
+            'а', 'б', 'в', 'г', 'ґ', 'д', 'е', 'є', 'ж', 'з', 'и', 'і', 'ї', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ю', 'я'};
+    int plusLetters;
+    int num = letters.length - 1;
+    String inputFileCod = "d:\\resolveProject\\FileInput.txt";
+    String outputFileCod = "d:\\resolveProject\\File.txt";
 
-        Character[] letters = {'.', ',', '”', ':', '-', '—', '!', '?', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-                'А', 'Б', 'В', 'Г', 'Ґ', 'Д', 'Е', 'Є', 'Ж', 'З', 'И', 'І', 'Ї', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь', 'Ю', 'Я',
-                'а', 'б', 'в', 'г', 'ґ', 'д', 'е', 'є', 'ж', 'з', 'и', 'і', 'ї', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ю', 'я'};
-        String inputFile = null;
-        Path path;
-        int plusLetters = 3;
-        int num = letters.length - 1;
-        try {
-            inputFile = "d:\\resolveProject\\FileInput.txt";
-            FileReader fr = new FileReader(inputFile);
+    String inputFileDecod = "d:\\resolveProject\\File.txt";
+    String outputFileDecod = "d:\\resolveProject\\FileOutputDecod.txt";
+
+    private void coder(int plusLetters) throws IOException {
+        this.plusLetters = plusLetters;
+        try (FileReader fr = new FileReader(inputFileCod);
+             FileWriter fw = new FileWriter(outputFileCod)) {
             while (fr.ready()) {
                 char ch = (char) fr.read();
-                for (int i = 0; i <= num; i++)
+                for (int i = 0; i <= num; i++) {
                     if (ch == letters[i]) {
                         char resLet = letters[i];
                         if (resLet == letters[num - plusLetters + 1])
-                            System.out.print(letters[0]);
+                            fw.append(letters[0]);
                         else if (resLet == letters[num - plusLetters + 2])
-                            System.out.print(letters[1]);
+                            fw.append(letters[1]);
                         else if (resLet == letters[num])
-                            System.out.print(letters[2]);
-                        else System.out.print(letters[i + plusLetters]);
+                            fw.append(letters[2]);
+                        else fw.append(letters[i + plusLetters]);
                     }
+                }
             }
-
         } catch (NoSuchFileException e) {
-            System.out.println("file with name " + inputFile + " do not exit");
+            System.out.println("file with name " + inputFileCod + " do not exit");
         }
+    }
+
+    private void decoder(int plusLetters) throws IOException {
+        try (FileReader fr = new FileReader(inputFileDecod);
+             FileWriter fw = new FileWriter(outputFileDecod)) {
+            while (fr.ready()) {
+                char ch = (char) fr.read();
+                for (int i = 0; i <= num; i++) {
+                    if (ch == letters[i]) {
+                        char resLet = letters[i];
+                        if (resLet == letters[0])
+                            fw.append(letters[num - plusLetters + 1]);
+                        else if (resLet == letters[1])
+                            fw.append(letters[num - plusLetters + 2]);
+                        else if (resLet == letters[2])
+                            fw.append(letters[num]);
+                        else fw.append(letters[i - plusLetters]);
+                    }
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        InputFile file = new InputFile();
+        file.coder(3);
+        file.decoder(3);
+
     }
 }
